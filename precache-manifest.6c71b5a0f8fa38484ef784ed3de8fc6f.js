1c16c35bb5561d30c40a6812ea98a9f4f1a471a4
vvxforce <vvxforce@gmail.com>
2020-01-08 10:23:06 +0200

    Updates

---
+++ precache-manifest.6c71b5a0f8fa38484ef784ed3de8fc6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5359606acc9e387046e4c96cbbdd8a79",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "1b43b460308e20db6658",
+    "url": "/react_people-table/static/css/main.865c7a71.chunk.css"
+  },
+  {
+    "revision": "d7afcb0fced3a22804de",
+    "url": "/react_people-table/static/js/2.03d71e74.chunk.js"
+  },
+  {
+    "revision": "1b43b460308e20db6658",
+    "url": "/react_people-table/static/js/main.c97b5324.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.3d8c6b1e7697442ca25d7e26bf334fd1.js"
+  "/react_people-table/precache-manifest.6c71b5a0f8fa38484ef784ed3de8fc6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_people-table/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
